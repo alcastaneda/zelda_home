594dd7c632d5737ea6bf233a31913dafc0bc76a7
Adriana Castaneda <adriana.lcs316@gmail.com>
2018-02-08 09:25:51 -0600

    added time of day

--- songTime.py
+++ songTime.py
@@ -5,23 +5,27 @@ def human_time(datetime_obj=datetime.now()):
     
     hr = int(datetime_obj.hour)
     mi = int(datetime_obj.minute)
-    
+    tod = " a m"
+
     singles = ["one", "two", "three", "four",
             "five", "six", "seven", "eight", "nine","ten", "eleven","twelve"]
     teens = ["ten", "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
     tens = ["twenty","thirty", "forty","fifty"]
     
+    if(hr>12):
+        hr = hr/2
+        tod = " p m"
     if(mi==0):
         human_str = singles[hr-1]
     else:
         div = mi / 10
         if(mi<10):
-            human_str = singles[hr-1] + " oh " + singles[mi-1]
+            human_str = singles[hr-1] + " oh " + singles[mi-1]+tod
         if(div==1):
-            human_str = singles[hr-1] +" "+ teens[mi-10] 
+            human_str = singles[hr-1] +" "+ teens[mi-10]+tod 
         else:
             rem = mi%10
-            human_str = singles[hr-1] +" "+ tens[div-2] +" "+ singles[rem-1]
+            human_str = singles[hr-1] +" "+ tens[div-2] +" "+ singles[rem-1]+tod
 
     return human_str
 
